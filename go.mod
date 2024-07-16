1e1d5ff4d998b512bfc829e97edb2f754934b2c4
krisjanisp <krisjanispetrucena@gmail.com>
2024-07-16 17:52:15 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/programme-lv/import-lio-task-script
+
+go 1.22.4
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -ex
+
+# Define the directory and script name
+SCRIPT_NAME="import-lio-task-script"
+INSTALL_DIR="/usr/local/bin"
+
+# Ensure go.mod exists, or initialize the Go module
+if [ ! -f "go.mod" ]; then
+    go mod init "$SCRIPT_NAME"
+fi
+
+# Get the necessary dependency
+go get github.com/pelletier/go-toml/v2
+
+# Build the Go script
+go build -o "$SCRIPT_NAME" ./cmd
+
+# Install the built script to /usr/local/bin
+sudo mv "$SCRIPT_NAME" "$INSTALL_DIR"
+
+# Ensure the script is executable
+sudo chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
+
+echo "Installation complete. You can now run the script using '$SCRIPT_NAME'."
